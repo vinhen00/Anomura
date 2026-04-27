fd47bb0a6f0a9824c4d33981685a4547acd256af
EldritchEli <Eli.uhlin@gmail.com>
2026-04-27 10:18:07 +0200

    sequence impl and refactor

--- crates/context/src/lib.rs
+++ crates/context/src/lib.rs
@@ -23,6 +23,7 @@ use std::cell::RefCell;
 use std::collections::{HashMap, HashSet};
 use std::mem;
 
+<<<<<<< HEAD
 pub enum CtxState {
     Builder(ContextBuilder),
     Ctx(GlobalContext),
@@ -409,29 +410,60 @@ pub enum NodeKind {
     Mock,
     Sequence,
 }
-//
+=======
+pub use crate::closure_wrappers::{ConditionDoublePointer, ReturnValDoublePointer};
+use crate::{
+    errors::{MockError, PredicateError},
+    types::{
+        edges::{Edge, EdgeTransitionInfo},
+        mock::{MockHead, MockId, MockState},
+        nodes::{Node, NodeIndex, Nodes},
+        sequences::{SequenceHead, SequenceHeads, SequenceIndex},
+        slices::Slices,
+    },
+};
 
+pub static GLOBAL_CONTEXT: OnceLock<Mutex<GlobalContext>> = OnceLock::new();
+//Slices are (for now) defined as sequences with a fixed start and end point.
+>>>>>>> 313fef3 (sequence impl and refactor)
+//
+#[derive(Debug)]
 pub struct GlobalContext {
     slices: Slices,
-    sequences: Vec<SequenceHead>,
+    sequences: SequenceHeads,
     mock_heads: HashMap<MockId, MockHead>,
     nodes: Nodes,
-    edges: Vec<Edge>,
 }
 
 impl GlobalContext {
-    pub fn get_dot(&self) -> String {
-        let dot = petgraph::dot::Dot::new(&self.graph);
-        format!("{dot:?}")
+    pub fn get_node_mut(&mut self, node_index: NodeIndex) -> Option<&mut Node> {
+        self.nodes.get_node_mut(node_index)
     }
+    pub fn get_node_ref(&mut self, node_index: NodeIndex) -> Option<&Node> {
+        self.nodes.get_node_ref(node_index)
+    }
+    pub fn get_dot(&self) -> String {
+        //let dot = petgraph::dot::Dot::new(&self.graph);
+        //format!("{dot:?}")
+        todo!()
+    }
+<<<<<<< HEAD
     pub unsafe fn into_fn<Input>(&self) -> Result<&dyn Fn(&Input) -> Result<()>> {
         let thick_ptr: *mut dyn Fn(&Input) -> Result<()> = unsafe { *(self.thin_ptr as *const _) };
         let ptr_ref: &dyn Fn(&Input) -> Result<()> =
             unsafe { thick_ptr.as_ref().expect("failed to cast thick_ptr to ref") };
         Ok(ptr_ref)
+=======
+    pub fn get_sequence_head(&self, index: SequenceIndex) -> Option<&SequenceHead> {
+        self.sequences.edge_ref(index)
+    }
+    pub fn mut_sequence_head(&mut self, index: SequenceIndex) -> Option<&mut SequenceHead> {
+        self.sequences.edge_mut(index)
+>>>>>>> 313fef3 (sequence impl and refactor)
     }
 }
 
+<<<<<<< HEAD
 pub struct ContextBuilder {
     mocks: HashMap<MockId, MockBuilder>,
     graph: DiGraph<MockNode, Edge>,
@@ -444,6 +476,79 @@ impl ContextBuilder {
             graph,
             mocks: HashMap::new(),
         }
+=======
+    pub fn enter_sequence(&mut self, seq_head_index: SequenceIndex) -> Result<()> {
+        let seq_head_vec = self
+            .get_sequence_head(seq_head_index)
+            .unwrap()
+            .effected_mocks
+            .clone();
+        for id in &seq_head_vec {
+            let mock_head_index = match self.mock_heads.get(id).map(|h| &h.state) {
+                Some(MockState::Locked {
+                    sequence_head_index,
+                }) => {
+                    return Err(format!(
+                        "mock with id {:?} wanted to enter sequence with index {:?} 
+                        but it was already in sequence with index{:?}",
+                        id, seq_head_index, sequence_head_index
+                    )
+                    .into());
+                }
+                Some(MockState::Unlocked { mock_head_index }) => *mock_head_index,
+                None => {
+                    return Err(format!(
+                        "mock_id {:?} not found when trying to enter sequence with index {:?}",
+                        id, seq_head_index
+                    )
+                    .into());
+                }
+            };
+            let mut instant_stack: Vec<NodeIndex> = vec![mock_head_index];
+            let mut visited: HashSet<NodeIndex> = HashSet::new();
+            let mut sequence_node_found = false;
+            while !sequence_node_found && let Some(node_index) = instant_stack.pop() {
+                let Some(node) = self.get_node_ref(node_index) else {
+                    panic!(
+                        "no node with id {:?} found when trying to enter sequence with index {:?}",
+                        &id, seq_head_index
+                    )
+                };
+
+                sequence_node_found = node.iter_conditions().any(|e| match e {
+                    Edge::SequenceEnter(sequence_head_index)
+                        if *sequence_head_index == seq_head_index =>
+                    {
+                        true
+                    }
+                    Edge::Instant { target, .. } => {
+                        if !visited.contains(&target) {
+                            instant_stack.push(*target);
+                            visited.insert(*target);
+                        }
+                        false
+                    }
+                    _ => false,
+                });
+            }
+            if !sequence_node_found {
+                return Err(format!(
+                    "Mock with id {:?} couldn't find a valid entry point for entering sequence{:?}",
+                    id, seq_head_index
+                )
+                .into());
+            }
+        }
+        // All related mocks were in the correct position, so we move their heads and return success
+        for id in seq_head_vec {
+            self.mock_heads.entry(id.clone()).and_modify(|h| {
+                h.state = MockState::Locked {
+                    sequence_head_index: seq_head_index,
+                }
+            });
+        }
+        Ok(())
+>>>>>>> 313fef3 (sequence impl and refactor)
     }
 
     pub fn finish(self) -> GlobalContext {
@@ -500,8 +605,8 @@ impl ContextBuilder {
         &mut self,
         mock_id: MockId,
         input: Input,
-    ) -> Result<ReturnVal> {
-        let (node_index, maybe_sequence_head) = match self
+    ) -> errors::Result<ReturnVal> {
+        let (node_index, maybe_sequence_head_ids) = match self
             .mock_heads
             .get(&mock_id)
             .map(|mock_head| &mock_head.state)
@@ -509,12 +614,17 @@ impl ContextBuilder {
             Some(MockState::Locked {
                 sequence_head_index,
             }) => {
-                let seq_head = self.get_sequence_head(*sequence_head_index).clone();
-                (seq_head.node_index, Some(seq_head))
+                let seq_head = self
+                    .get_sequence_head(*sequence_head_index)
+                    .clone()
+                    .unwrap();
+                //This is bad performance and memory consumption wise... lets try to find a way to avoid cloning here.
+                (seq_head.node_index, Some(seq_head.effected_mocks.clone()))
             }
             Some(MockState::Unlocked { mock_head_index }) => (*mock_head_index, None),
             None => return Err(MockError::NoMatchingId),
         };
+<<<<<<< HEAD
         let new_node_index = self.graph.add_node(MockNode {
             entry: false,
             exit,
@@ -688,26 +798,27 @@ impl From<&str> for MockError {
         let mut sequence_head_indices = vec![];
         let mut sequence_stack_append = vec![];
         let mut instant_stack_append = vec![];
+=======
+        let mut sequence_head_indices: Vec<(NodeIndex, SequenceIndex)> = vec![];
+        let mut sequence_stack_append: Vec<NodeIndex> = vec![];
+        let mut instant_stack_append: Vec<NodeIndex> = vec![];
+>>>>>>> 313fef3 (sequence impl and refactor)
         let mut node_index_stack = vec![node_index];
         let mut visited = vec![];
         let mut failed_conditionals: Vec<PredicateError> = vec![];
         let mut successful_conditionals: Vec<_> = vec![];
-        //traverses graph until
+        //traverses graph until we find a valid condition
         while let Some(node_index) = node_index_stack.pop() {
-            let Some(res) = self.graph.node_weight(node_index) else {
-                return Err("node not found".into());
-            };
-            if res.ids.contains(&mock_id) {
-                return Err(format!(
-                    "node with index {:?} expected {:?} but received {:?}",
-                    node_index, mock_id, res.ids
-                )
-                .into());
-            };
-            self.graph
-                .edges_directed(node_index, petgraph::Direction::Outgoing)
-                .for_each(|e| match e.weight() {
-                    Edge::Instant { .. } => instant_stack_append.push(e.target()),
+            if let Some(node) = self.get_node_ref(node_index) {
+                if !node.contains_id(&mock_id) {
+                    return Err(format!(
+                        "node with index {:?} expected {:?} but received {:?}",
+                        node_index, mock_id, node.ids
+                    )
+                    .into());
+                };
+                node.iter_conditions().for_each(|e| match e {
+                    Edge::Instant { target, .. } => instant_stack_append.push(*target),
                     Edge::Condition(conditional_edge) => {
                         let condition = unsafe { conditional_edge.condition.into_fn::<Input>() };
                         let res = condition(&input);
@@ -715,7 +826,7 @@ impl From<&str> for MockError {
                             Ok(()) => successful_conditionals.push(EdgeTransitionInfo {
                                 priority: conditional_edge.priority,
                                 return_val: conditional_edge.return_val.clone(),
-                                target_node: e.target(),
+                                target_node: conditional_edge.target,
                             }),
                             Err(e) => {
                                 failed_conditionals.push(e);
@@ -723,25 +834,22 @@ impl From<&str> for MockError {
                         }
                     }
                     Edge::SequenceEnter(index) => sequence_head_indices.push((node_index, *index)),
-                    Edge::SequenceExit(exit_mock_id)
-                        if maybe_sequence_head
+                    Edge::SequenceExit { id, target }
+                        if maybe_sequence_head_ids
                             .clone()
-                            .map(|head| head.effected_mocks.contains(exit_mock_id))
+                            .map(|ids| ids.contains(id))
                             .unwrap_or(false) =>
                     {
-                        if *exit_mock_id == mock_id {
-                            sequence_stack_append.push(e.target())
+                        if *id == mock_id {
+                            sequence_stack_append.push(*target)
                         }
-                        self.mock_heads
-                            .entry(exit_mock_id.clone())
-                            .and_modify(|head| {
-                                head.state = MockState::Unlocked {
-                                    mock_head_index: e.target(),
-                                }
-                            });
                     }
                     _ => {}
                 });
+            } else {
+                return Err("node not found".into());
+            };
+
             for (target, seq_head) in sequence_head_indices.drain(0..) {
                 if let Ok(()) = self.enter_sequence(seq_head) {
                     sequence_stack_append.push(target)
@@ -761,9 +869,9 @@ impl From<&str> for MockError {
                 );
                 let return_val = unsafe { return_val_ptr.into_fn::<Input, ReturnVal>()(input) };
                 let new_node_index = edge.target_node;
-                if self.graph.node_weight(new_node_index).is_none() {
+                if self.get_node_ref(new_node_index).is_none() {
                     return Err(format!("new node index {:?} is invalid", new_node_index).into());
-                };
+                }
                 self.mock_heads.entry(mock_id.clone()).and_modify(|h| {
                     h.state = MockState::Unlocked {
                         mock_head_index: new_node_index,
